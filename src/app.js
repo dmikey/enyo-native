2e82394101a458ad5eb2a50d9df909fa64a3ea1c
Derek Anderson <derek.anderson@lge.com>
2015-06-24 09:20:21 -0700

    cleanup source app

--- src/app.js
+++ src/app.js
@@ -1,67 +1,59 @@
 var
-	kind = require('enyo/kind');
-
+    kind = require('enyo/kind');
 var
-	Application = require('./core/qtApp'),
-	Window = require('./core/qtWindow'),
-	Label = require('./ui/qtLabel'),
-	Image = require('./ui/qtImage'),
-	Sound = require('./ui/qtSound'),
-	Pixmap = require('./ui/qtPixmap'),
-	Color = require('./ui/qtColor');
-
+	Component
 var
-	colorPixmap = kind({
-		kind: Pixmap,
-		width: 125,
-		height: 125,
-		left: 30,
-		top: 200,
-		components: [
-			{
-				name: 'fill',
-				kind: Color,
-				r: 255,
-				g: 0,
-				b: 0
-			}
-		]
-	});
+    Application = require('./core/qtApp'),
+    Window = require('./core/qtWindow'),
+    Label = require('./ui/qtLabel'),
+    Image = require('./ui/qtImage'),
+    Sound = require('./ui/qtSound'),
+    Pixmap = require('./ui/qtPixmap'),
+    Color = require('./ui/qtColor');
+
+//example of a pixmap fill
+//create a standalone so that the pixmap
+//control owns the `fill`
+var
+    colorPixmap = kind({
+        kind: Pixmap,
+        width: 125,
+        height: 125,
+        left: 30,
+        top: 200,
+        components: [{
+            name: 'fill',
+            kind: Color,
+            r: 255,
+            g: 0,
+            b: 0
+        }]
+    });
 
 module.exports = kind({
-	kind: Application,
-	components: [
-		{
-			kind: Window,
-			width: 640,
-			height: 480,
-			components: [
-				{
-					kind: Label,
-					left: 20,
-					top: 20,
-					content: 'Hello World From Enyo!'
-				},
-				{
-					kind: Image,
-					left: 20,
-					top: 35,
-					src: 'resources/beanbird.png'
-				},
-				{
-					kind: Sound,
-					autoPlay: true,
-					src: 'resources/answer.wav'
-				},
-				{
-					kind: colorPixmap
-				}
+    kind: Application,
+    components: [{
+        kind: Window,
+        width: 640,
+        height: 480,
+        components: [{
+                kind: Label,
+                left: 20,
+                top: 20,
+                content: 'Hello World From Enyo!'
+            }, {
+                kind: Image,
+                left: 20,
+                top: 35,
+                src: 'resources/beanbird.png'
+            }, {
+                kind: Sound,
+                autoPlay: false,
+                src: 'resources/answer.wav'
+            }, {
+                kind: colorPixmap
+            }
 
-		]
-		}
-	],
-	fillPixmap: function (pixmap) {
-		console.log('fill');
-		return pixmap;
-	}
+        ]
+    }]
 });
